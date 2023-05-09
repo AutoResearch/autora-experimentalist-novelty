b19d44621ff8f5a81f1cb64f74655e9c19ff6c26
Sebastian Musslick <sebastian@musslick.de>
2023-05-09 05:17:25 -0400

    test: added novelty score test

--- tests/test_exp_novelty_sampler.py
+++ tests/test_exp_novelty_sampler.py
@@ -1,4 +1,4 @@
-from autora.experimentalist.sampler.novelty import novelty_sampler
+from autora.experimentalist.sampler.novelty import novelty_sampler, novelty_score_sampler
 import numpy as np
 
 # Note: We encourage you to write more functionality tests for your sampler.
@@ -44,3 +44,16 @@ def test_novelty_sampler_ND():
     assert reordered_matrix1.shape[0] == 2
     assert reordered_matrix1.shape[1] == 3
     assert np.array_equal(reordered_matrix1, np.array([[10, 11, 12], [7, 8, 9]]))
+
+def test_novelty_score_sampler_ND():
+    # define two matrices
+    matrix1 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])
+    matrix2 = np.array([[1, 1, 1], [2, 2, 2], [3, 3, 3]])
+    num_samples = 3
+
+    # reorder matrix1 according to its distances to matrix2, and obtain distance score
+    (reordered_matrix1, score) = novelty_score_sampler(condition_pool=matrix1,
+                                        reference_conditions=matrix2,
+                                        num_samples=num_samples)
+
+    assert score[0] > score[1]  and score[1] > score[2]
